6195c869caed7868ba8c20e21b6ad46ec0d617c0
Paloma Arce <palomaarces@outlook.com>
2021-03-15 20:53:23 -0300

    Adicionando código de insert

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -45,10 +45,7 @@ class Usuario{
         if(isset($result[0])){
             //verificar se tem pelo menos um dado
             $row = $result[0];
-            $this->setIdUsuario($row['idUsuario']);
-            $this->setLogin($row['deslogin']);
-            $this->setSenha($row['dessenha']);
-            $this->setDtCadastro(new Datetime($row['dataCadastro']));
+            $this->setData($result[0]);
         }
         //com o código acima, preenchemos os dados com os dados vindo do banco
         //carregamos os dados do banco para o objeto
@@ -62,6 +59,71 @@ class Usuario{
             "dessenha: " =>$this->getSenha(),
             "dtCadastro : " => $this->getDtCadastro()->format("d/m/Y H:i:s")
         ));
+    }
+
+    public static function source($login){
+        //buscar um usuario
+        $sql = new Sql();
+        $sql->select("SELECT * FROM usuarios WHERE deslogin LIKE :SEARCH", array(
+            ':SEARCH'=>'%$login%'
+            //isso para buscar qualquer coisa antes e qualquer coisa depois de $login
+        ));
+    }
+
+    public static function getList(){
+        //pode ser static pq não utilizamos $this
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM usuarios ORDER BY deslogin;");
+    }
+
+    public function login($login, $senha){
+         //carregar pelo id
+         $sql = new Sql();
+         $result = $sql->select("SELECT * FROM usuarios WHERE deslogin =:LOGIN AND dessenha = :PASSWORD", array(
+             ":LOGIN" => $login,
+             ":PASSWORD" => $senha
+         ));
+ 
+         if(isset($result[0])){
+             //verificar se tem pelo menos um dado
+             $row = $result[0];
+             $this->setData($result[0]);
+           
+         } else{
+             throw new Exception(
+            "Login ou senha inválidos"
+             );
+         }
+    }
+
+    public function setData($data){
+        $this->setIdUsuario($data['idUsuario']);
+        $this->setLogin($data['deslogin']);
+        $this->setSenha($data['dessenha']);
+        $this->setDtCadastro(new Datetime($data['dataCadastro']));
+    }
+
+    public function insert(){
+        $sql = new Sql();
+        $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+            ":LOGIN " => $this->getLogin(), 
+            ":PASSWORD " => $this->getSenha()
+            //pegamos do objeto
+            //chamando o precedure, conseguimos retornar o id
+        ));
+        //chamando uma procedure, um procedimento
+        if(count(($sql) > 0)){
+            $this->setData($sql[0]);
+        }
+    }
+
+    public function __construct($login="", $senha="")
+    //colocando ="" nos parametros faz com que ele não seja obrigatorio, posso chamar tanto passando parametros quando não
+    //passando, se não passar, vai preencher com ""
+    {
+        $this->setLogin($login);
+        $this->setSenha($senha);
+        //assim coloando no construtor facilita a criação, diminui as linhas
     }
 
 }
--- DAO/index.php
+++ DAO/index.php
@@ -1,10 +1,33 @@
 <?php
 require_once("config.php");
 //arquivo com o autoload para carregar as classes necessárias
-$root = new Usuario();
-$root->loadById(2);
+//carega um só usuario
+//$root = new Usuario();
+//$root->loadById(2);
 
-echo $root;
+//echo $root;
 //vai funcionar por causa do toString
+
+//carregando uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carregando uma lista de usuarios buscando pelo login
+//$busca = Usuario::source("u");
+
+//echo json_encode($busca);
+
+//carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "157849");
+//echo $usuario;
+
+
+//inserindo usuariio
+$aluno = new Usuario("aluno", "password");
+//$aluno->setLogin("aluno");
+//$aluno->setSenha("@luno");
+$aluno ->insert();
+echo $aluno;
 
 ?>
